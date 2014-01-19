525b5056f890dfa1b5ff59d2f4458c7640ecb1e7
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-01-19 23:10:03 +0900

    associative arrayを大幅に書き換え

--- associative.d
+++ associative.d
@@ -1,80 +1,60 @@
 // Written in the D programming language.
-
 /**
-Functions acting upon associative arrays: filtering them, etc.
+convert AA to range, and merge AA
 
-License:   <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
-Authors:   Philippe Sigaud
-
-Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+Authors:    Kazuki Komatsu(k3_kaimu)
+License:    NYSL
 */
 module dranges.associative;
 
+import dranges.functional;
+
 import std.algorithm,
        std.range,
+       std.traits,
        std.typecons;
 
 version(unittest) import std.stdio;
 
-/**
-Simple helper function to duplicate an associative array.
-*/
-@property
-V[K] dup(K,V)(V[K] aa)
-{
-    V[K] _aa;
-    foreach(k,v; aa) _aa[k] = v;
-    return _aa;
-}
 
 /**
 There may be a need for an 'asRange' function that takes a container
 and returns a range iterating over the entire content. In this particular case, asRange takes
 an associative array of type V[K] and returns (K,V) tuples.
-It's a random-access range, with a length. opIndexAssign is defined and will
-change the original associative array.
-Note:
-I'm a bit ambivalent on this one. front/back returns tuples, because that's the
-entire content and because if you only want keys or values, you can use the homonym
-properties. But for opIndex (indexed on keys) you already know the key, so it returns
-only a value. The same for opIndexApply. I'm wondering if that's OK or not.
-Example:
-----
-auto aa = ["Hello":5, "World!":6, "a":99, "":0, "Howdy":6];
-auto aa_range = aa.asRange;
-assert(equal(aa_range, [tuple("a",99), tuple("",0), tuple("Hello",5), tuple("World!",6), tuple("Howdy",6)][]));
-assert(aa_range.length == 5); // has a length
-assert(aa_range["Hello"] == 5); // opIndex. Returns a value.
-aa_range["Hello"] = 100; // opIndexAssign.
-assert(aa["Hello"] == 100); // changes the original AA.
-aa["Howdy"] = 66; // If we change the original AA after .asRange was called
-assert(aa_range["Howdy"] == 66); // The range presents the udpated view.
-----
 */
-struct AsRange(K,V)
+auto asRange(AA)(AA aa)
+if(isAssociativeArray!(AA))
 {
-    V[K] _aa;
-    K[] _keys;
+    static struct AsRange(AA)
+    if(isAssociativeArray!(Unqual!AA))
+    {
+        bool empty() @property
+        {
+            return _ks.empty;
+        }
 
-    this(V[K] aa) { _aa = aa/+.dup+/; _keys = _aa.keys;}
 
-    bool empty() { return _keys.empty;}
-    Tuple!(K,V) front() { return tuple(_keys.front, _aa[_keys.front]);}
-    @property AsRange save() { return this;}
-    void popFront() { _keys.popFront;}
-    Tuple!(K,V) back() { return tuple(_keys.back, _aa[_keys.back]);}
-    void popBack() { _keys.popBack;}
-    size_t length() { return _keys.length;}
-    V opIndex(K key) { return _aa[key];}
-    void opIndexAssign(V value, K key) { _aa[key] = value;}
+        auto front() @property
+        {
+            return tuple(_ks.front, _aa[_ks.front]);
+        }
+
+
+        void popFront()
+        {
+            _ks.popFront();
+        }
+
+      private:
+        AA _aa;
+        typeof(AA.init.byKey()) _ks;
+    }
+
+
+    return AsRange!AA(aa, aa.byKey);
 }
 
-/// ditto
-AsRange!(K,V) asRange(K,V)(V[K] aa) {
-    return AsRange!(K,V)(aa);
-}
-
+///
 unittest
 {
     scope(failure) writefln("unittest Failure :%s(%s)", __FILE__, __LINE__);
@@ -83,149 +63,46 @@ unittest
     auto aa = ["Hello":5, "World!":6, "a":99, "":0, "Howdy":6];
     auto aa_range = aa.asRange;
     assert(is(ElementType!(typeof(aa_range)) == Tuple!(string, int)));
-//    assert(equal(aa_range, [tuple("a",99), tuple("",0), tuple("Hello",5), tuple("World!",6), tuple("Howdy",6)][]));
-    assert(aa_range.length == 5); // has a length
-    assert(aa_range["Hello"] == 5); // opIndex. Returns a value.
-    aa_range["Hello"] = 100; // opIndexAssign.
-    assert(aa["Hello"] == 100); // changes the original AA.
-    aa["Howdy"] = 66; // If we change the original AA after .asRange was called
-    assert(aa_range["Howdy"] == 66); // The range presents the udpated view.
+    assert(equal(aa_range, aa.keys.zip(aa.values)));
 }
 
-///
-Unqual!(ElementType!VR)[ElementType!KR]
-toAssoc(KR, VR)(KR keysRange, VR valuesRange)
-if (isInputRange!KR && isInputRange!VR)
-{
-    typeof(return) aa;
-    foreach(key; keysRange) {
-        if (valuesRange.empty) break;
-        aa[key] = valuesRange.front;
-        valuesRange.popFront;
-    }
-    return aa;
-}
-
-///
-Unqual!(ElementType!VR)[ElementType!KR]
-addToAssoc(KR, VR)(Unqual!(ElementType!VR)[ElementType!KR] aa, KR keysRange, VR valuesRange)
-if (isInputRange!KR && isInputRange!VR)
-{
-    foreach(key; keysRange) {
-        if (valuesRange.empty) break;
-        aa[key] = valuesRange.front;
-        valuesRange.popFront;
-    }
-    return aa;
-}
-
-///
-Unqual!(ElementType!VR)[ElementType!KR]
-addToAssoc(KR, VR)(Unqual!(ElementType!VR)[ElementType!KR] aa, KR keysRange, VR valuesRange, ElementType!VR delegate(ElementType!VR, ElementType!VR) onCollision)
-if (isInputRange!KR && isInputRange!VR)
-{
-    foreach(key; keysRange) {
-        if (valuesRange.empty) break;
-        if (key in aa) {
-            aa[key] = onCollision(aa[key], valuesRange.front); // (old,new)
-        }
-        else {
-            aa[key] = valuesRange.front;
-        }
-        valuesRange.popFront;
-    }
-    return aa;
-}
-
-///
-V[K] append(K,V)(V[K] aa, K key, V value)
-{
-    aa[key] = value;
-    return aa;
-}
-///
-V[K] prepend(K,V)(V[K] aa, K key, V value)
-{
-    aa[key] = value;
-    return aa;
-}
-
-/// Filtering on keys
-V[K] filterOnKeys(alias pred,K,V)(V[K] aa)
-{
-    V[K] result;
-    foreach(k,v; aa) if (unaryFun!pred(k)) { result[k] = v;}
-    return result;
-}
-
-/**
-A constraint template to detect associative arrays of a certain type. It's a curried (nested) template:
-isKV!(int,string) is itself a template.
-Example:
-----
-alias isKV(int,string) IntString;
-assert(IntString!(string[int]));
-assert(!IntString!(double[int]));
-----
-*/
-template isKV(K,V)
-{
-    template isKV(T) {
-        static if (is(T == V[K]))
-            enum bool isKV = true;
-        else
-            enum bool isKV = false;
-    }
-}
-
-/// Merge AA.
-V[K] merge(K,V,T...)(V[K] aa, T aas) if(allSatisfy!(isKV!(K,V), T))
-{
-    auto result = aa;
-    foreach(i,Type; T)
-    {
-        foreach(k,v; aas[i])
-        {
-            result[k] = v;
-        }
-    }
-    return result;
-}
 
 /// Merge AA, with a merging function if some keys exist on more than one array.
-V[K] mergeWith(alias fun = "c", K,V,T...)(V[K] aa, T aas) if(allSatisfy!(isKV!(K,V), T))
+AA merge(alias fn = "c", AA, size_t N)(AA[N] aas...)
+if(N >= 2)
 {
-    auto result = aa;
-    foreach(i,Type; T)
-    {
-        foreach(k,v; aas[i])
-        {
-            if (k in result) {
-                result[k] = naryFun!fun(k,result[k],v);
+    AA* l = &(aas[0]),
+        s;
+
+    foreach(i; 1 .. N){
+        s = &(aas[1]);
+
+        if((*s).length > (*l).length)
+            swap(s, l);
+
+        foreach(k, ref v; *s){
+            if(k in (*l)){
+                auto r = naryFun!fn(k, (*l)[k], v);
+                (*l)[k] = r;
             }
-            else {
-                result[k] = v;
-            }
+            else
+                (*l)[k] = v;
         }
     }
-    return result;
+
+    return *l;
 }
 
 ///
-ElementType!R.Types[1][ElementType!R.Types[0]] toAA(R)(R r) if (isForwardRange!R && is(ElementType!R KV == Tuple!(K,V), V,K))
-{
-    typeof(return) aa;
-    foreach(elem; r) aa[elem.field[0]] = elem.field[1];
-    return aa;
-}
-///
-ElementType!VR[ElementType!KR] toAA(KR, VR)(KR keyRange, VR valueRange) if (isForwardRange!KR && isForwardRange!VR)
-{
-    typeof(return) aa;
-    foreach(elem; keyRange) {
-        if (valueRange.empty) break;
-        aa[elem] = valueRange.front;
-        valueRange.popFront;
-    }
-    return aa;
+unittest {
+    auto aa1 = ["Hello":5, "World!":6],
+         aa2 = ["a":99, "":0, "Howdy":6];
+
+    assert(merge(aa1, aa2) == ["Hello":5, "World!":6, "a":99, "":0, "Howdy":6]);
+    assert(merge(aa1, typeof(aa2).init) == aa1);
+    assert(merge(typeof(aa1).init, aa2) == aa2);
+
+    aa1 = ["Hello":5, "World!":6];
+    aa2 = ["Hello":10, "W":2];
+    assert(merge!"b+c"(aa1, aa2) == ["Hello":15, "World!":6, "W":2]);
 }
